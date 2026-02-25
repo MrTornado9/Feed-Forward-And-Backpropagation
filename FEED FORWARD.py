237e2681abea0fd52dfcd3458c0cb263470fec6a
Kareem Esa <ahuesa7@gmail.com>
2026-02-25 21:18:40 +0200

    Add files via upload

---
+++ FEED FORWARD.py
@@ -0,0 +1,54 @@
+import numpy as np
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+def sigmoid_derivative(x):
+    return x * (1 - x)
+
+inputs = np.array([[0,0], [0,1], [1,0], [1,1]])
+expected_output = np.array([[0], [1], [1], [0]])
+
+input_neurons = 2
+hidden_neurons = 2
+output_neurons = 1
+
+
+np.random.seed(42)
+hidden_weights = np.random.uniform(size=(input_neurons, hidden_neurons))
+hidden_bias = np.random.uniform(size=(1, hidden_neurons))
+output_weights = np.random.uniform(size=(hidden_neurons, output_neurons))
+output_bias = np.random.uniform(size=(1, output_neurons))
+
+epochs = 10000
+learning_rate = 0.1
+
+for epoch in range(epochs):
+    # -------------------------
+    # 1. Feed Forward
+    # -------------------------
+    hidden_layer_activation = np.dot(inputs, hidden_weights) + hidden_bias
+    hidden_layer_output = sigmoid(hidden_layer_activation)
+
+    output_layer_activation = np.dot(hidden_layer_output, output_weights) + output_bias
+    predicted_output = sigmoid(output_layer_activation)
+
+    # -------------------------
+    # 2. Backpropagation
+    # -------------------------
+    error = expected_output - predicted_output
+    d_predicted_output = error * sigmoid_derivative(predicted_output)
+
+    error_hidden_layer = d_predicted_output.dot(output_weights.T)
+    d_hidden_layer = error_hidden_layer * sigmoid_derivative(hidden_layer_output)
+
+    
+    output_weights += hidden_layer_output.T.dot(d_predicted_output) * learning_rate
+    output_bias += np.sum(d_predicted_output, axis=0, keepdims=True) * learning_rate
+    
+    hidden_weights += inputs.T.dot(d_hidden_layer) * learning_rate
+    hidden_bias += np.sum(d_hidden_layer, axis=0, keepdims=True) * learning_rate
+
+
+print("result :")
+print(predicted_output)
